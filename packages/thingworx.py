47b05a9b9d11162caf39a3e0a33e0a938c91cad9
Brennan <ackerman.brennan@gmail.com>
2018-04-06 09:04:22 -0600

    adding thingworx query methods

--- packages/thingworx.py
+++ packages/thingworx.py
@@ -3,15 +3,30 @@ import requests
 
 import config
 
-PUT_HEADERS = {
+HEADERS = {
         "Content-Type": "application/json",
         "Authorization": config.HTTP_BASIC_AUTH,
-        "Cache-Control": "no-cache"
+        "Cache-Control": "no-cache",
+        "appKey": config.APP_KEY
         }
 
 def putDataToThing(thingName, propertyName, data):
     url = "http://{}/Thingworx/Things/{}/Properties/{}".format(config.THINGWORX_HOST, thingName, propertyName)
-    querystring = {"appKey": config.APP_KEY}
     payload = str("{\"" + propertyName + "\":" + str(data) + "}")
-    response = requests.request("PUT", url, data=payload, headers=PUT_HEADERS, params=querystring)
+    response = requests.request("PUT", url, data=payload, headers=HEADERS)
+    print(url + "--" + str(response.status_code))
+    return response.status_code
+
+
+def createThing(thingName):
+    url = "http://{}/Thingworx/Resources/EntityServices/Services/CreateThing".format(config.THINGWORX_HOST)
+    payload = str("{\"name\":" + "\"" + thingName + "\",\"thingTemplateName\": \"RemoteThing\"}")
+    response = requests.request("POST", url, data=payload, headers=HEADERS)
+    print(url + "--" + str(response.status_code))
+    return response.status_code
+
+def getNamesOfThings():
+    url = "http://{}/Thingworx/Things".format(config.THINGWORX_HOST)
+    response = requests.request("GET", url, headers=HEADERS)
+    print(url + "--" + str(response.status_code))
     return response.status_code
