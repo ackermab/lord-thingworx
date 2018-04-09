74c69aef16b8bdb65ac853fc4769859989fa4bb7
Brennan Ackerman <ackerman.brennan@gmail.com>
2018-04-09 11:47:39 -0600

    rework config to use json file

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import packages.mscl as mscl
 import packages.lord as lord
-import config
+import json
 
 from tkinter import *
 
 class StationConfig:
-    def __init__(self, parent):
+    def __init__(self, parent, config):
         self.parent = parent
         self.frame = Frame(self.parent)
-        self.com_port = None
-        self.baud_rate = None
+        self.config = config
         self.initUI()
 
     def initUI(self):
@@ -19,40 +18,28 @@ class StationConfig:
         
         self.e1 = Entry(self.parent)
         self.e2 = Entry(self.parent)
-        self.e1.insert(0, config.basestation["com_port"])
-        self.e2.insert(0, config.basestation["baud_rate"])
+        self.e1.insert(0, self.config["com_port"])
+        self.e2.insert(0, self.config["baud_rate"])
         self.e1.grid(row=0, column=1)
         self.e2.grid(row=1, column=1)
         
         Button(self.parent, text='Add BaseStation', command=self.setBaseStation).grid(row=2, column=0, sticky=W)
 
     def setBaseStation(self):
-        self.com_port = self.e1.get()
-        self.baud_rate = self.e2.get()
+        self.config["com_port"] = self.e1.get()
+        self.config["baud_rate"] = self.e2.get()
         self.parent.destroy()
 
-    def getComPort(self):
-        return self.com_port
+    def getUpdatedConfig(self):
+        return config
 
-    def getBaudRate(self):
-        return self.baud_rate
 
 class NodeConfig:
-    def __init__(self, parent, index):
+    def __init__(self, parent, node):
         self.parent = parent
         self.frame = Frame(self.parent)
+        self.node = node
         self.done = False
-        self.index = index
-        if index < len(config.basestation["nodes"]):
-            self.node_addr = config.basestation["nodes"][index]["address"]
-            self.node_type = config.basestation["nodes"][index]["type"]
-            self.thing_name = config.basestation["nodes"][index]["thingName"]
-            self.thing_properties = config.basestation["nodes"][index]["thingProperties"]
-        else:
-            self.node_addr = ""
-            self.node_type = ""
-            self.thing_name = ""
-            self.thing_properties = ""
         self.initUI()
 
     def initUI(self):
@@ -66,17 +53,18 @@ class NodeConfig:
         self.e2 = Entry(self.parent)
         self.e3 = Entry(self.parent)
         self.e4 = Entry(self.parent)
-        self.e1.insert(0, self.node_addr)
-        self.e2.insert(0, self.node_type)
-        self.e3.insert(0, self.thing_name)
-        self.e4.insert(0, self.thing_properties)
+        self.e1.insert(0, self.node["node_addr"])
+        self.e2.insert(0, self.node["node_type"])
+        self.e3.insert(0, self.node["thing_name"])
+        self.e4.insert(0, self.node["thing_properties"])
         self.e1.grid(row=0, column=1)
         self.e2.grid(row=1, column=1)
         self.e3.grid(row=2, column=1)
         self.e4.grid(row=3, column=1)
 
-        Button(self.parent, text='Add Another Node', command=self.addAnother).grid(row=4, column=0, sticky=W)
-        Button(self.parent, text='Done Adding Nodes', command=self.completeAdding).grid(row=4, column=1, sticky=W)
+        Button(self.parent, text='Add and Add Another', command=self.addAnother).grid(row=4, column=0, sticky=W)
+        Button(self.parent, text='Add and Finish Nodes', command=self.completeAdding).grid(row=4, column=1, sticky=W)
+        Button(self.parent, text='Cancel', command=self.concelAdding).gri(row=4, column=2, sticky=W)
 
     def addAnother(self):
         self.setValues()
@@ -87,31 +75,28 @@ class NodeConfig:
         self.done = True
         self.parent.destroy()
 
+    def cancelAdding(self):
+        self.node = None
+        self.done = True
+        self.parent.destroy()
+
     def setValues(self):
-        self.node_addr = self.e1.get()
-        self.node_type = self.e2.get()
-        self.thing_name = self.e3.get()
-        self.thing_properties = self.e4.get()
+        self.node["node_addr"] = self.e1.get()
+        self.node["node_type"] = self.e2.get()
+        self.node["thing_name"] = self.e3.get()
+        self.node["thing_properties"] = self.e4.get()
 
     def getNode(self):
-        node = {}
-        node["address"] = self.node_addr
-        node["type"] = self.node_type
-        node["thingName"] = self.thing_name
-        node["thingProperties"] = self.thing_properties
-        return node
+        return self.node
 
     def getDone(self):
         return self.done
 
 class ThingWorxConfig:
-    def __init__(self, parent):
+    def __init__(self, parent, config):
         self.parent = parent
         self.frame = Frame(self.parent)
-        self.serverIp = None
-        self.appKey = None
-        self.username = None
-        self.password = None
+        self.config = config
         self.initUI()
 
     def initUI(self):
@@ -124,11 +109,11 @@ class ThingWorxConfig:
         self.e1 = Entry(self.parent)
         self.e2 = Entry(self.parent)
         self.e3 = Entry(self.parent)
-        self.e4 = Entry(self.parent)
-        self.e1.insert(0, config.THINGWORX_HOST)
-        self.e2.insert(0, config.APP_KEY)
-        self.e3.insert(0, config.HTTP_USERNAME)
-        self.e4.insert(0, config.HTTP_PASSWORD)
+        self.e4 = Entry(self.parent, show="*")
+        self.e1.insert(0, self.config["thingworx_host"])
+        self.e2.insert(0, self.config["app_key"])
+        self.e3.insert(0, self.config["http_username"])
+        self.e4.insert(0, "")
         self.e1.grid(row=0, column=1)
         self.e2.grid(row=1, column=1)
         self.e3.grid(row=2, column=1)
@@ -137,30 +122,29 @@ class ThingWorxConfig:
         Button(self.parent, text='Configure Basestation', command=self.setConfig).grid(row=4, column=0, sticky=W)
 
     def setConfig(self):
-        self.serverIp = self.e1.get()
-        self.appKey = self.e1.get()
-        self.username = self.e1.get()
-        self.password = self.e1.get()
-        self.closeConfig()
-
-    def closeConfig(self):
+        self.config["thingworx_host"] = self.e1.get()
+        self.config["app_key"] = self.e2.get()
+        self.config["http_basic_auth"] = createBasicAuth(self.e3.get(), self.e4.get()) # We shouldn't really save this
+        self.config["http_username"] = self.e3.get()
         self.parent.destroy()
 
-    def getConfig(self):
-        config = {
-            "server_ip": self.serverIp,
-            "app_key": self.appKey,
-            "username": self.username,
-            "password": self.password
-            }
-        return config
+    def createBasicAuth(self, username, password):
+        basic = b64encode(bytes(username + ":" + password, "utf-8")
+        basic = "Basic " + basic
+        print(basic)
+        return basic
+
+    def getUpdatedConfig(self):
+        return self.config
 
 def readConfig():
-    return
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    return config
 
-def updateConfig(com_port, baud_rate, nodes, tw_config):
-    print("Storing new config", com_port, baud_rate, nodes, tw_config)
-    return
+def updateConfig(config):
+    with open('config.json', 'w') as f:
+        json.dump(config, f)
 
 def main():
     print("Initializing connection to Lord Sensors and ThingWorx")
@@ -170,36 +154,36 @@ def main():
 
     # Base Station Config Window
     win1 = Tk()
-    app1 = StationConfig(win1)
+    app1 = StationConfig(win1, config)
     win1.mainloop()
-    com_port = app1.getComPort()
-    baud_rate = app1.getBaudRate()
+    config = app1.getUpdatedConfig()
 
+    print(config["nodes"])
     # Loop of Node Config Windows
-    nodes = []
-    index = 0
-    while True:
+    for node in config["nodes"]:
         win2 = Tk()
-        app2 = NodeConfig(win2, index)
+        app2 = NodeConfig(win2, node)
         win2.mainloop()
+        if app2.getNode() is not None:
+            node = app2.getNode()
         if app2.getDone():
             break
-        else:
-            nodes.append(app2.getNode())
-            index = index + 1
+
+    config["nodes"] = nodes
+    print(config["nodes"])
 
     # ThingWorx Server Config Window
     win3 = Tk()
-    app3 = ThingWorxConfig(win3)
+    app3 = ThingWorxConfig(win3, config)
     win3.mainloop()
-    tw_config = app3.getConfig()
+    config = app3.getUpdatedConfig()
 
     # Store config back to config file
-    updateConfig(com_port, baud_rate, nodes, tw_config)
+    updateConfig(config)
 
     try:
         # Connect base station
-        bs = lord.connectToBaseStation(com_port, baud_rate)
+        bs = lord.connectToBaseStation(config["com_port"], config["baud_rate"])
         if bs:
             # Create network
             network = mscl.SyncSamplingNetwork(bs)
@@ -208,19 +192,19 @@ def main():
                 for node in nodes:
                     # Determine type of node
                     if node["type"] == "force":
-                        n = lord.ForceNode(node["address"], node["type"], node["thingName"], node["thingProperties"])
+                        n = lord.ForceNode(node["address"], node["type"], node["thing_name"], node["thing_properties"])
                     elif node["type"] == "temp":
-                        n = lord.TempNode(node["address"], node["type"], node["thingName"], node["thingProperties"])
+                        n = lord.TempNode(node["address"], node["type"], node["thing_name"], node["thing_properties"])
                 ##### Sample new node type #####
                 #   elif node["type"] == "foo":
-                #       n = lord.FooNode(node["address"], node["type"], node["thingName"], node["thingProperties"])
+                #       n = lord.FooNode(node["address"], node["type"], node["thing_name"], node["thing_properties"])
                 ##### Copy above section without comments and change foo to type of node
                     else:
                         continue # Node with type not recognized is skipped
 
                     # Check ThingWorx Server for node with the same name, add if it doesn't exist
                     tw_nodes = getNamesOfThings()
-                    if node["thingName"] in tw_nodes:
+                    if node["thing_name"] in tw_nodes:
                         print("Thing already exists on ThingWorx Server, skipping create")
                     else:
                         node.createThing()
